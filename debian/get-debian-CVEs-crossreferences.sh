bb025a67494ac95986cfa3a2e1d780fa1ff24112
Kurt Seifried <kurt@seifried.org>
2022-03-10 14:44:48 -0700

    Debian cleanup

--- debian/get-debian-CVEs-crossreferences.sh
+++ debian/get-debian-CVEs-crossreferences.sh
@@ -2,11 +2,18 @@
 
 # Run from /gsd-database/ root directory
 
-# rm -f crossreferences.html
+rm -f crossreferences.html
+rm -f crossreferences2.html
 
-#wget https://www.debian.org/security/crossreferences.html
+wget https://www.debian.org/security/crossreferences.html
 # Format is 2 td entries, first is DSA, second is CVE(s)
-#grep "^<tr VALIGN=\"TOP\"><td><a href=\"https://www.debian.org/security/" crossreferences.html > crossreferences2.html
+grep "^<tr VALIGN=\"TOP\"><td><a href=\"https://www.debian.org/security/" crossreferences.html > crossreferences2.html
+
+FILENAME=debian-cve-crossreferences.csv
+rm -f $FILENAME
+touch $FILENAME
+
+TEMP=$(mktemp)
 
 while IFS= read -r line
 do
@@ -17,7 +24,13 @@ do
   do
     if echo "$element" | grep -q "^https://security-tracker.debian.org/tracker/CVE-"; then
       CVE_ID=`echo $element | sed 's/https:\/\/security-tracker\.debian\.org\/tracker\///'`
-      echo "$CVE_ID,$DSA_URL"
+      echo "$CVE_ID,$DSA_URL" >> $TEMP
     fi
   done
 done < crossreferences2.html
+
+sort -rn $TEMP > $FILENAME
+
+rm -f $TEMP
+rm -f crossreferences.html
+rm -f crossreferences2.html
