67bd2963c9806977e5171f1d0cb9dd915bc67342
Kurt Seifried <kurt@seifried.org>
2022-03-10 22:42:01 -0700

    mageia advisories

---
+++ mageia/get-mageia-advisories.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ubuntu OVAL data seems like the best bet
+
+wget https://advisories.mageia.org/8.html
+wget https://advisories.mageia.org/7.html
+wget https://advisories.mageia.org/6.html
+wget https://advisories.mageia.org/5.html
+wget https://advisories.mageia.org/4.html
+wget https://advisories.mageia.org/3.html
+wget https://advisories.mageia.org/2.html
+
+grep CVE *.html| awk -F"\"" '{print $3","$2}'| sed 's/^>//' | sed 's/<\/a>,/,https:\/\/advisories.mageia.org\//' | sort -n | uniq > mageia-cve-advisories.csv
+
+rm -f [0-9].html
