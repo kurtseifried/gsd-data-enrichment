aafdf6867c6f551038c321bba5f21cb2fe94dc70
Kurt Seifried <kurt@seifried.org>
2022-06-24 11:45:09 -0600

    updates

--- 1-update-gsd-database.sh
+++ 1-update-gsd-database.sh
@@ -6,7 +6,7 @@
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/archlinux/archlinux-cve-security-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/debian/debian-cve-crossreferences-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/mageia/mageia-cve-advisories-CURRENT.csv
-../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/oracle-linux/oracle-linux-cve-advisories-CURRENT.csv
+#../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/oracle-linux/oracle-linux-cve-advisories-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/redhat/redhat-cve-rhsamapcpe-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/suse/suse-cve-mainlist-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/ubuntu/ubuntu-cve-oval-CURRENT.csv
