972beb40a1882ab076111dee78b6ffbcc7b8735c
Kurt Seifried <kurt@seifried.org>
2022-06-23 21:00:38 -0600

    amazon-linux

--- 1-update-gsd-database.sh
+++ 1-update-gsd-database.sh
@@ -2,9 +2,11 @@
 
 # run from gsd-database directory
 
+../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/amazon-linux/amazon-linux-cve-mainlist-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/archlinux/archlinux-cve-security-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/debian/debian-cve-crossreferences-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/mageia/mageia-cve-advisories-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/redhat/redhat-cve-rhsamapcpe-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/suse/suse-cve-mainlist-CURRENT.csv
 ../gsd-data-enrichment/add-CVE-URL-CSV.sh ../gsd-data-enrichment/ubuntu/ubuntu-cve-oval-CURRENT.csv
+
---
+++ amazon-linux/get-amazon-linux-CVEs-mainlist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# https://www.suse.com/security/cve/
+# https://www.suse.com/security/cve/CVE-2022-0001.html
+# and convert to a list of CVEs in suse-cve.txt
+
+
+rm -f index.html
+
+touch amazon-linux-cve-mainlist.csv
+mv -f amazon-linux-cve-mainlist.csv amazon-linux-cve-mainlist-LASTONE.csv
+
+wget -q https://alas.aws.amazon.com/
+
+#
+# Grep all the CVE ID's out (one per line) and strip the CVE ID, then print in CSV format
+#
+#grep "^<a href=\"CVE-" index.html | cut -d"\"" -f2 | sed 's/\.html$//' | awk -F" " '{print $1",https://www.suse.com/security/cve/"$1".html"}'> suse-cve-mainlist.csv
+
+grep "'/cve/html/CVE" index.html  | cut -d"'" -f4  | awk -F" " '{print $1",https://alas.aws.amazon.com"$1}'| sed 's/^\/cve\/html\///' > amazon-linux-cve-mainlist.csv 
+
+cat amazon-linux-cve-mainlist-LASTONE.csv amazon-linux-cve-mainlist-LASTONE.csv amazon-linux-cve-mainlist.csv | sort -n | uniq -u > amazon-linux-cve-mainlist-CURRENT.csv
